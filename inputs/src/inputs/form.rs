515decaf679cfa3a2748de72cb8afeb2bb4ad66f
soapydev <alexandre.beauchamp.thibault@gmail.com>
2024-11-20 19:35:15 -0500

    Form can have variable amount of padding

--- inputs/src/inputs/form.rs
+++ inputs/src/inputs/form.rs
@@ -5,9 +5,29 @@ use leptos::{Children, IntoView, MaybeSignal};
 pub enum FormBoxStyle {
     SimpleLogin,
 }
+
+#[derive(PartialEq, Clone, Copy)]
+pub enum Padding {
+    None,
+    Small,
+    Medium,
+    Large,
+}
+
+#[derive(PartialEq, Clone, Copy)]
+pub enum Level {
+    Dp1,
+    Dp2,
+    Dp3,
+    Dp4,
+    Dp5,
+    Dp6,
+    Dp7,
+}
 #[component]
 pub fn FormBox(
     #[prop(default = FormBoxStyle::SimpleLogin)] style: FormBoxStyle,
+    #[prop(default = Padding::Medium)] padding: Padding,
     #[prop(default = MaybeSignal::from(Some(String::from("Login")))
     )]
     title: MaybeSignal<Option<String>>,
@@ -20,6 +40,9 @@ pub fn FormBox(
         <div
             class="form"
             class:simple-login=style == FormBoxStyle::SimpleLogin
+            class:padding-small=padding == Padding::Small
+            class:padding-medium=padding == Padding::Medium
+            class:padding-large=padding == Padding::Large
         >
             <header class="form-header">
                 <img src={logo_src} alt={logo_alt}/>
